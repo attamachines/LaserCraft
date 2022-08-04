213971465efa240456bc2488f5710260df95de91
attamachines <110521315+attamachines@users.noreply.github.com>
2022-08-04 19:30:33 +0700

    Add files via upload

---
+++ netDxf/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿// Assembly netDxf, Version 2.0.2.0
+
+[assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
+[assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
+[assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.DisableOptimizations | System.Diagnostics.DebuggableAttribute.DebuggingModes.EnableEditAndContinue | System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | System.Diagnostics.DebuggableAttribute.DebuggingModes.Default)]
+[assembly: System.Reflection.AssemblyTitle("netDxf")]
+[assembly: System.Reflection.AssemblyDescription(".net Dxf Reader-Writer")]
+[assembly: System.Reflection.AssemblyConfiguration("")]
+[assembly: System.Reflection.AssemblyCompany("")]
+[assembly: System.Reflection.AssemblyProduct("netDxf")]
+[assembly: System.Reflection.AssemblyCopyright("Daniel Carvajal \x00a9  2016")]
+[assembly: System.Reflection.AssemblyTrademark("")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.Runtime.InteropServices.Guid("4b9680da-0121-489f-8a4f-e15ab0b04d6e")]
+[assembly: System.Reflection.AssemblyFileVersion("2.0.2.0")]
+[assembly: System.CLSCompliant(true)]
+
+
